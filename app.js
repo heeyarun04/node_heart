449387b7283a54ba944a71b6d2eaa32b1a8eaef1
prasetyoians <prasetyoians@gmail.com>
2023-10-10 15:48:30 +0700

    ini sudah diupdate, udah ada template html

--- app.js
+++ app.js
@@ -18,33 +18,10 @@ app.get('/', (req, res) => {
   res.status(200).json({msg: 'Hello this shaa!'})
 })
 
-app.get('/identitas/',require('./controller/response').identitas);
-app.get('/identitas/:id',require('./controller/response').penilaian);
-app.post('/kirimdata',require('./controller/response').kirimData );
-
-
-app.get("/weather", require("./controller/response").weather);
-app.get("/jsonspreadsheet", require("./controller/response").jsonSpreadsheet);
-
-app.post('/senddata',require('./controller/response').sendData );
-
-app.get('/coba', function(req, res){
-
-   res.sendFile(path.join(__dirname, '/coba.json'));
-});
-
-
-
-
-app.get('/tabel', require("./controller/response").wait);
-app.get('/fromdb', require("./controller/response").fromdb);
-app.get('/refgetdata', require("./controller/response").refGetData);
-app.get('/refsenddata', require("./controller/response").refSendData);
-app.get('/refgetlogdata', require("./controller/response").refGetLogData);
-app.post('/lamppost', require("./controller/response").lampPost);
-app.get('/lampget', require("./controller/response").lampGet);
 
 app.get('/senddatatosps', require("./controller/response").sendDataToSps);
+app.get('/getdata', require("./controller/response").getData);
+app.get('/reload', require("./controller/response").reload);
 
 
 
